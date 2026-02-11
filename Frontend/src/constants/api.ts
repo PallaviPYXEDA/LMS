497086fa71d940ddfd1b67ac07eee253b0520353
PallaviPYXEDA <149144280+PallaviPYXEDA@users.noreply.github.com>
2026-02-11 08:51:13 +0530

    Delete Frontend directory

--- Frontend/src/constants/api.ts
+++
@@ -1,5 +0,0 @@
-export const API_ENDPOINTS = {
-    GET_OUTPUT_JSON: 'create-output-json'
-  } as const;
-  
-  export const API_BASE_URL = 'https://4ykub62jxj.execute-api.us-east-1.amazonaws.com/Lms/';
